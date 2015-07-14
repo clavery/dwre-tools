221764f792c798ae4353e49021fa4d6645a20b63
Charles Lavery <clavery@pixelmedia.com>
2015-07-14 16:37:24 -0400

    validate command for schema validations

--- dwre_tools/cli.py
+++ dwre_tools/cli.py
@@ -4,10 +4,19 @@ import getpass
 import zipfile
 from .env import *
 from .tail import tail_logs
+from .validations import validate_command
 
 def list_cmd_handler(args):
-    print args.project
-    print "\t", args.env
+    raise NotImplementedError("not implemented yet")
+
+
+def migrate_cmd_handler(args):
+    print args.test
+    raise NotImplementedError("not implemented yet")
+
+
+def validate_cmd_handler(args):
+    validate_command(args.target)
 
 
 def get_env_password(env):
@@ -28,6 +37,7 @@ def tail_cmd_handler(args):
 parser = ArgumentParser(description="Demandware Tools")
 parser.add_argument('-p', '--project', help="DWRE Project Name")
 parser.add_argument('-e', '--env', help="DWRE Environment Name")
+
 cmd_parser = parser.add_subparsers(title="Commands")
 
 list_cmd = cmd_parser.add_parser('list', help="list DWRE environments")
@@ -37,6 +47,18 @@ tail_cmd = cmd_parser.add_parser('tail', help="tail logs")
 tail_cmd.set_defaults(func=tail_cmd_handler)
 tail_cmd.add_argument('-f', '--filters', help="logfile prefix filter [default 'warn,error,fatal']", default="warn,error,fatal")
 tail_cmd.add_argument('-i', help="refresh interval in seconds [default 5]", default=5, type=int)
+
+migrate_cmd = cmd_parser.add_parser('migrate', help="schema/data migrations")
+migrate_cmd.set_defaults(func=migrate_cmd_handler)
+migrate_cmd.add_argument('-n', dest="test", help="test run; do not execute migrations", action="store_true")
+migrate_cmd.set_defaults(test=False)
+migrate_cmd.add_argument('-d', '--dir', help="migrations directory (default: migrations)", default="migrations")
+migrate_cmd.set_defaults(test=False)
+
+validate_cmd = cmd_parser.add_parser('validate', help="XMLSchema validations")
+validate_cmd.set_defaults(func=validate_cmd_handler)
+validate_cmd.add_argument('target', help="filename or directory to validate")
+
 
 def main():
     args = parser.parse_args()
---
+++ dwre_tools/validations.py
@@ -0,0 +1,102 @@
+from lxml import etree as ET
+import os
+import re
+
+from colorama import Fore, Back, Style
+import dwre_tools
+
+SCHEMA_MAP = {
+    "abtest" : "abtest.xsd",
+    "bmext" : "bmext.xsd",
+    "cache-settings" : "cachesettings.xsd",
+    "catalog" : "catalog.xsd",
+    "coupons" : "coupon.xsd",
+    "coupon-redemptions" : "couponredemption.xsd",
+    "customers" : "customer.xsd",
+    "customer-groups" : "customergroup.xsd",
+    "customer-lists" : "customerlist.xsd",
+    "customerpaymentinstrument" : "customerpaymentinstrument.xsd",
+    "custom-objects" : "customobject.xsd",
+    "feeds" : "feed.xsd",
+    "form" : "form.xsd",
+    "geolocations" : "geolocation.xsd",
+    "gift-certificates" : "giftcertificate.xsd",
+    "inventory" : "inventory.xsd",
+    "library" : "library.xsd",
+    "metadata" : "metadata.xsd",
+    "oauth" : "oauth.xsd",
+    "orders" : "order.xsd",
+    "payment-settings" : "paymentmethod.xsd",
+    "payment-processors" : "paymentprocessor.xsd",
+    "preferences" : "preferences.xsd",
+    "pricebooks" : "pricebook.xsd",
+    "product-lists" : "productlist.xsd",
+    "promotions" : "promotion.xsd",
+    "redirect-urls" : "redirecturl.xsd",
+    "schedules" : "schedules.xsd",
+    "search" : "search.xsd",
+    "search2" : "search2.xsd",
+    "services" : "services.xsd",
+    "shipping" : "shipping.xsd",
+    "site" : "site.xsd",
+    "slot-configurations" : "slot.xsd",
+    "sort" : "sort.xsd",
+    "sourcecodes" : "sourcecode.xsd",
+    "stores" : "store.xsd",
+    "tax" : "tax.xsd",
+    "url-rules" : "urlrules.xsd",
+    "xml" : "xml.xsd"
+}
+
+
+def validate_xml(filename, throw=True):
+    xml = ET.parse(filename)
+    root_el = xml.getroot()
+    root_tag = root_el.tag[root_el.tag.find('}')+1:]
+    if root_tag not in SCHEMA_MAP:
+        raise RuntimeError("cannot find schema for %s" % root_tag)
+    schema_name = SCHEMA_MAP[root_tag]
+    schema = ET.XMLSchema(file=os.path.join(dwre_tools.__path__[0], 'schemas', schema_name))
+    schema.validate(xml)
+    if throw:
+        schema.assertValid(xml)
+    return schema
+
+
+SCHEMALINE_RE = re.compile(r'^(.*?:\d+:\d+:)')
+def validate_file(full_filename):
+    print "Validating %s"  % full_filename,
+    try:
+        schema = validate_xml(full_filename, throw=False)
+        if schema.error_log:
+            print Fore.RED + "[ERROR]" + Fore.RESET
+            for e in schema.error_log:
+                print SCHEMALINE_RE.sub(Fore.BLUE + '\\1' + Fore.RESET, str(e))
+        else:
+            print Fore.GREEN + "[OK]" + Fore.RESET
+    except ET.XMLSyntaxError as e:
+        print Fore.RED + "[ERROR]" + Fore.RESET
+        print e
+
+
+def validate_directory(directory):
+    for (dirpath, dirnames, filenames) in os.walk(directory):
+        for fname in filenames:
+            (root, ext) = os.path.splitext(fname)
+
+            if ext != ".xml":
+                continue
+
+            full_filename = os.path.join(dirpath, fname)
+            validate_file(full_filename)
+
+
+
+def validate_command(target):
+    if os.path.isdir(target):
+        validate_directory(target)
+    elif os.path.isfile(target):
+        validate_file(target)
+    else:
+        raise IOError("file not found")
+
