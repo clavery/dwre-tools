6151876a469c2fca50309b2b95f3c85fc9631b28
Charles Lavery <charles.lavery@gmail.com>
2016-03-16 09:07:00 -0400

    1.1.1 update schames 16.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
 
 setup(
     name='dwre-tools',
-    version='1.1.0',
+    version='1.1.1',
     description='Demandware tools',
     author='Charles Lavery',
     author_email='clavery@pixelmedia.com',
