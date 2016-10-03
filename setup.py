32c4d3f864fa0ef77f537a435f3819f7a449a29b
Charles Lavery <clavery@pixelmedia.com>
2016-10-03 10:40:29 -0400

    update to 16.9 schemas

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
 
 setup(
     name='dwre-tools',
-    version='1.3.5',
+    version='1.3.6',
     description='Demandware tools',
     author='Charles Lavery',
     author_email='clavery@pixelmedia.com',
